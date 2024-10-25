8d4ee5a42e1ef9a626316c54b9644711e90973b7
unknown <unknown@localhost>
2024-10-25 13:28:25 +0200

    Demonstrate constants in demo file

--- test.s
+++ test.s
@@ -1,15 +1,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; This is a simple program which outputs the ASCII table once.       ;;
+;; This is a simple program which outputs the ASCII table.            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	.ORG	1000
 
+XCSR	=	#177564
+XBUF	=	#177566
+
 _START:
-LOOP:	TSTB	@#177564
+LOOP:	TSTB	@#XCSR
 	BPL	LOOP
 	CMP	R0,	#40
 	BLO	NEXT
 	CMP	R0,	#177
 	BHIS	NEXT
-	MOVB	R0,	@#177566
+	MOVB	R0,	@#XBUF
 NEXT:	INC	R0
 	JMP	LOOP
